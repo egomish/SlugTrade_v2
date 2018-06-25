5695436aa1e7a70111ff502407a8d5dac1f5d05f
Emma Gomish <egomish@ucsc.edu>
2018-06-25 15:29:46 -0700

    modified start scripts to accept a port number as a command-line argument

--- slug_trade/first_start.sh
+++ slug_trade/first_start.sh
@@ -1,3 +1,3 @@
 ./venv_setup.sh
 ./renew_db.sh
-./start.sh
+./start.sh $1
--- slug_trade/start.sh
+++ slug_trade/start.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
+PORT="$1"
+
+if [ PORT == "" ]; then
+    $PORT="8000"
+fi
+
 source env/bin/activate
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py runserver
+python3 manage.py runserver $PORT
